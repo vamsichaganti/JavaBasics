4bf759f85be1205ba628ee7b95463fb7e51587ff
vamsichaganti <vamsichaganti@dbs.com>
2022-07-18 19:38:51 +0530

    constructor class

--- src/com/basics/training/Car.java
+++ src/com/basics/training/Car.java
@@ -5,6 +5,12 @@ public class Car {
 	public Car() {
 		System.out.println("Car constructor called..!!");
 	}
+	
+	public Car(String name,int seats) {
+		System.out.println("Two Parameter constructor called..!!");
+		this.name=name;
+		this.seats=seats;		
+	}
 	//class level variables
 	// static & non static or instance 
 	String name;
--- src/com/basics/training/CarDemoApp.java
+++ src/com/basics/training/CarDemoApp.java
@@ -7,12 +7,14 @@ public class CarDemoApp {
 		Car c1 = new Car();
 		c1.setName("white car");
 		c1.setSeats(5);
-		Car c2 = new Car();
-		c2.setName("red car");
-		c2.setSeats(7);
+		Car c2 = new Car("red car",7);
+//		c2.setName("red car");
+//		c2.setSeats(7);
+		c2.setName("yellow car");
 		
 		System.out.println("My "+c1.getName()+" seat capacity is:"+c1.getSeats());
 		System.out.println("My "+c2.getName()+" seat capacity is:"+c2.getSeats());
 	}
 
 }
+
